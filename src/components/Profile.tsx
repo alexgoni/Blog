ad14962b4220733815aa5416299c7d4a26a18b19
alexgoni <goni000211@gmail.com>
2024-01-16 01:50:17 +0900

    feat: context 도입

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,11 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
-import React from "react";
+import React, { useContext } from "react";
 import { toast } from "react-toastify";
 
 export default function Profile() {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext);
 
   const onSignOut = async () => {
     try {
@@ -22,10 +23,8 @@ export default function Profile() {
       <div className="profile">
         <div className="profile__info">
           <div className="profile__img" />
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">
-            {auth?.currentUser?.displayName || "사용자"}
-          </div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "사용자"}</div>
         </div>
         <div
           role="presentation"
