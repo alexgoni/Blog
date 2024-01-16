9f6ecc8d4993ec5cb7e6503f353637befa4d4e2c
alexgoni <goni000211@gmail.com>
2024-01-17 01:39:53 +0900

    feat: read 기능 구현

--- src/components/post/PostDetail.tsx
+++ src/components/post/PostDetail.tsx
@@ -1,29 +1,61 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { PostProps } from "./PostList";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "components/Loader";
 
 export default function PostDetail() {
+  const params = useParams();
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
   return (
     <>
       <div className="post__detail">
-        <div className="post__box">
-          <div className="post__title">제목</div>
-          <div className="post__profile-box">
-            <div className="post__user-img" />
-            <div className="post__author">alexgoni</div>
-            <div className="post__date">2024.01.13 일요일</div>
-          </div>
-          <div className="post__utils-box">
-            <div className="post__edit">
-              <Link to={`/posts/edit/1`}>수정</Link>
+        {post ? (
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
+            <div className="post__profile-box">
+              <div className="post__user-img" />
+              <div className="post__author">{post?.email}</div>
+              <div className="post__date">{post?.createdAt}</div>
             </div>
-            <div className="post__delete">삭제</div>
+            <div className="post__utils-box">
+              <div className="post__edit">
+                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+              </div>
+              <div
+                className="post__delete"
+                role="presentation"
+                onClick={handleDelete}
+              >
+                삭제
+              </div>
+            </div>
+            <div className="post__content post__text--pre-wrap">
+              {post?.content}
+            </div>
           </div>
-          <div className="post__content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime unde
-            fugit animi corrupti iusto voluptate aliquam commodi laboriosam
-            voluptatibus eius? Maiores quis ipsa exercitationem sapiente itaque
-            natus vero distinctio ex?
-          </div>
-        </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface PostListProps {
@@ -7,8 +10,32 @@ interface PostListProps {
 
 type TabType = "all" | "my";
 
+export interface PostProps {
+  id?: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createdAt: string;
+}
+
 export default function PostList({ hasNavigation = true }: PostListProps) {
   const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  const getPosts = async () => {
+    const datas = await getDocs(collection(db, "posts"));
+
+    datas?.forEach((doc) => {
+      const dataObj = { ...doc.data(), id: doc.id };
+      setPosts((prev) => [...prev, dataObj as PostProps]);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <>
@@ -32,28 +59,32 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
       )}
 
       <div className="post-list">
-        {[...Array(10)].map((e, index) => (
-          <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>
-              <div className="post__profile-box">
-                <div className="post__user-img" />
-                <div className="post__author">alexgoni</div>
-                <div className="post__date">2024.01.13 일요일</div>
-              </div>
-              <div className="post__title">게시글 {index}</div>
-              <div className="post__content">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                unde fugit animi corrupti iusto voluptate aliquam commodi
-                laboriosam voluptatibus eius? Maiores quis ipsa exercitationem
-                sapiente itaque natus vero distinctio ex?
-              </div>
-              <div className="post__utils-box">
-                <div className="post__edit">수정</div>
-                <div className="post__delete">삭제</div>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {posts?.length > 0 ? (
+          posts?.map((post, index) => (
+            <div key={post?.id} className="post__box">
+              <Link to={`/posts/${post?.id}`}>
+                <div className="post__profile-box">
+                  <div className="post__user-img" />
+                  <div className="post__author">{post?.email}</div>
+                  <div className="post__date">{post?.createdAt}</div>
+                </div>
+                <div className="post__title">{post?.title}</div>
+                <div className="post__content">{post?.summary}</div>
+              </Link>
+              {post?.email === user?.email && (
+                <div className="post__utils-box">
+                  <Link to={`/posts/edit/${post?.id}`} className="post__edit">
+                    수정
+                  </Link>
+
+                  <div className="post__delete">삭제</div>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div className="post__no-post">게시글이 없습니다.</div>
+        )}
       </div>
     </>
   );
